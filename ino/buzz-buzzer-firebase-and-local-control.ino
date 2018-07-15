cbc92e6be8b1efad20f196d8a9c2b14c46ac512f
SirFizX <eeisaman@auburnschl.edu>
2018-07-15 19:10:30 -0400

    remove AP Code

--- ino/buzz-buzzer-firebase-and-local-control.ino
+++ ino/buzz-buzzer-firebase-and-local-control.ino
@@ -1,40 +1,13 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 #define FIREBASE_HOST "YOUR_FIREBASE_PROJECT.firebaseio.com"
 #define FIREBASE_AUTH "YOUR KEY HERE"
 #define WIFI_SSID "YOUR_WIFI_NETWORK_SSID"
 #define WIFI_PASSWORD "YOUR_WIFI_NETWORK_PASSWORD"
 #define buzzerPin D2
-ESP8266WebServer server;
+
 //password must be at least 8 characters
 const char WiFiAPPSK[] = "YOUR_ESP8266_PASSWORD";
-
-const char INDEX_HTML[] =
-  "<!DOCTYPE HTML>"
-  "<html>"
-  "<head>"
-  "<meta name = \"viewport\" content = \"width = device-width, initial-scale = 1.0, maximum-scale = 1.0, user-scalable=0\">"
-  "<title>ESP8266</title>"
-  "<style>"
-    "body { background-color: #808080; font-family: Arial, Helvetica, Sans-Serif; Color: Maroon; }"
-  "</style>"
-  "</head>"
-  "<body>"
-  "<h1>ESP8266 Direct Access Demo</h1>"
-  "<button onclick='toggleLED()'>Toggle LED</button>"
-  "<button onclick='buzzBuzzer()'>Buzz Buzzer</button>"
-  "<script>"
-    "function toggleLED(){"
-      "fetch('/toggleLED').then(stream=>stream.text()).then(text=>console.log(text))"
-    "}"
-    "function buzzBuzzer(){"
-      "fetch('/buzzBuzzer').then(stream=>stream.text()).then(text=>console.log(text))"
-    "}"
-  "</script>"
-  "</body>"
-  "</html>";
 
 void setup()
 {
@@ -44,16 +17,16 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT); 
   digitalWrite(LED_BUILTIN,LOW);
 
-  server.on("/",sendIndex);
-  server.on("/toggleLED", toggleLED);
-  server.on("/buzzBuzzer", buzzBuzzer);
-  server.begin();
-  Serial.println("");
-  Serial.println("");
-  Serial.print("Server running on http://192.168.4.1/");
-
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.setString("buzz", "false");
+  Firebase.setBool("share/buzz", false);
+  if(Firebase.success()){
+    Serial.print("Firebase write success!");
+  } else {
+    Serial.print("Firebase write failed!");
+    Serial.println("");
+    Serial.print(Firebase.error());
+  }
+  Serial.println("");
 }
 
 int timePassed (int time) {
@@ -67,41 +40,27 @@ int timePassed (int time) {
 }
 
 int checkFirebaseTime = 0;
-String buzz = "false";
+bool buzz = false;
 void loop()
 {
   if (timePassed (checkFirebaseTime) >= 2000) {
-    buzz = Firebase.getString("buzz");
-    if(buzz == "true") buzzBuzzer();
+    buzz = Firebase.getBool("share/buzz");
+    if(buzz && Firebase.success()) buzzBuzzer();
     checkFirebaseTime = millis();
   }
-  server.handleClient();
-}
-
-void sendIndex(){
-  server.send(200,"text/html",INDEX_HTML);  
-}
-
-void toggleLED(){
-  digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
-  //server.send(204,"");
-  server.send(200,"text/plain","Toggle LED!\n");
 }
 
 void buzzBuzzer(){
-  Firebase.setString("buzz", "false");
+  Firebase.setBool("buzz", false);
   //tone( pin number, frequency in hertz, duration in milliseconds);
   tone(buzzerPin,2000,1000);
   delay(2000);
-  tone(buzzerPin,0);
-  //server.send(204,"");
-  server.send(200,"text/plain","Buzz Buzzer!\n");
+  digitalWrite(buzzerPin,LOW);
 }
 
 void setupWiFi()
 {
-  WiFi.mode(WIFI_AP_STA);
-  //Set up Access Point
+  WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
@@ -111,22 +70,4 @@ void setupWiFi()
   Serial.println();
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
-  
-  // Set up Station
-  // Do a little work to get a unique-ish name. Append the
-  // last two bytes of the MAC (HEX'd) to "Thing-":
-  uint8_t mac[WL_MAC_ADDR_LENGTH];
-  WiFi.softAPmacAddress(mac);
-  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
-                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
-  macID.toUpperCase();
-  String AP_NameString = "ELHS ESP8266 " + macID;
-
-  char AP_NameChar[AP_NameString.length() + 1];
-  memset(AP_NameChar, 0, AP_NameString.length() + 1);
-
-  for (int i=0; i<AP_NameString.length(); i++)
-    AP_NameChar[i] = AP_NameString.charAt(i);
-  //AP_NameChar is the SSID for the Wireless Access Point
-  WiFi.softAP(AP_NameChar, WiFiAPPSK);
 }
